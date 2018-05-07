5c71f3cbcf8fe57cdc1603974d48f296dfb9b7f3
Sun <1960176680@qq.com>
2018-05-07 13:13:17 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/customview/MyView.java
@@ -0,0 +1,59 @@
+package com.example.administrator.customview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2016-12-05.
+ */
+
+public class MyView extends View {
+
+    private Paint mPaint;
+    private Context mContext;
+    private static final String mString = "Welcome to Mr Wei's blog";
+
+    public MyView(Context context) {
+        super(context);
+        mPaint = new Paint();
+    }
+
+    public MyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mPaint = new Paint();
+
+        TypedArray a = context.obtainStyledAttributes(attrs,
+                R.styleable.MyView);
+
+        int textColor = a.getColor(R.styleable.MyView_textColor,
+                0XFFFFFFFF);
+        float textSize = a.getDimension(R.styleable.MyView_textSize, 36);
+
+        mPaint.setTextSize(textSize);
+        mPaint.setColor(textColor);
+        a.recycle();
+    }
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+//设置填充
+        mPaint.setStyle(Paint.Style.FILL);
+
+        //画一个矩形,前俩个是矩形左上角坐标，后面俩个是右下角坐标
+        canvas.drawRect(new Rect(10, 10, 100, 100), mPaint);
+
+        mPaint.setColor(Color.BLUE);
+        //绘制文字
+        canvas.drawText(mString, 10, 110, mPaint);
+
+
+
+
+    }
+}
